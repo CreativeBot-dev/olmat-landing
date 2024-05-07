e74e967664f60a92eaa86423801b006eaa60d9a6
mchramonz <mchrama27@gmail.com>
2024-05-07 16:50:13 +0700

    Update: daftar button

--- src/app/component/Intro.jsx
+++ src/app/component/Intro.jsx
@@ -9,7 +9,7 @@ import Image from "next/image";
 
 export default function paralax() {
   return (
-    <div className="flex bg-gradient-to-t from-tema2-0 to-white justify-center -mb-60">
+    <div className="flex justify-center bg-gradient-to-t from-tema2-0 to-white -mb-60">
       <MouseParallaxContainer
         className="parallax"
         containerStyle={{
@@ -19,30 +19,33 @@ export default function paralax() {
         }}
         globalFactorX={0.2}
         globalFactorY={0.2}
-        resetOnLeave>
-        <div className="grid sm:grid-cols-1 lg:grid-cols-2 gap-0 md:flex-row px-4 md:px-14 sm:px-6">
-          <div className="flex flex-col row-start-2 lg:row-start-auto justify-center lg:justify-center lg:pr-11">
-            <h1 className="text-5xl md:text-8xl lg:text-7xl font-bold font-Adlam text-tema1-0">
+        resetOnLeave
+      >
+        <div className="grid gap-0 px-4 sm:grid-cols-1 lg:grid-cols-2 md:flex-row md:px-14 sm:px-6">
+          <div className="flex flex-col justify-center row-start-2 lg:row-start-auto lg:justify-center lg:pr-11">
+            <h1 className="text-5xl font-bold md:text-8xl lg:text-7xl font-Adlam text-tema1-0">
               OLMAT UINSA
             </h1>
-            <h2 className="text-4xl md:text-7xl font-bold font-Adlam text-tema1-0">
+            <h2 className="text-4xl font-bold md:text-7xl font-Adlam text-tema1-0">
               2024
             </h2>
 
             <p className="lg:text-xl mt-[3%]">
-              &quot;Explore Mathematics Skills with ASIC: Action, Spirit, Intelligence, and Competitive&quot;
+              &quot;Explore Mathematics Skills with ASIC: Action, Spirit,
+              Intelligence, and Competitive&quot;
             </p>
-            <div className="grid place-items-center my-10 py-2 md:my-7 ">
+            <div className="grid py-2 my-10 place-items-center md:my-7 ">
               <a
-                className="py-2 px-8 bg-tema1-0 rounded-full text-2xl  text-white animate-bounce"
-                href="https://daftar.olmat-uinsa.com/"
-                target="blank">
+                className="px-8 py-2 text-2xl text-white rounded-full bg-tema1-0 animate-bounce"
+                href="https://daftar.olmatuinsa.online/"
+                target="blank"
+              >
                 Daftar
               </a>
             </div>
           </div>
 
-          <div className="flex flex-col  row-span-1 items-center relative pt-5 lg:px-16 justify-center">
+          <div className="relative flex flex-col items-center justify-center row-span-1 pt-5 lg:px-16">
             <div className="absolute z-0">
               <MouseParallaxChild factorX={0.5} factorY={0.5}>
                 <Image
